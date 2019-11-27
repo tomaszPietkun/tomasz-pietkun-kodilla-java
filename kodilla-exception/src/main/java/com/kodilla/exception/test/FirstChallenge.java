96b4bb8213285bcceb4cb3b7b40ea7d03dadc65a
tomaszPietkun <tomasz.pietkun.99@gmail.com>
2019-11-27 14:24:17 +0100

    TryCatchFinallyTask

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,39 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if(b == 0){
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        try {
+
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+
+        } catch (ArithmeticException e) {
+
+            double result = 0;
+            System.out.println("It is impossible to divide by 0! Error:" + e);
+        }
+
+        finally {
+
+            System.out.println("Put different b number!");
+        }
+
+
+    }
+}
+
